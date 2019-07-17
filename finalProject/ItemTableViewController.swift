2de6669978b01308874beaf254340c71d12587cf
Apple <apple@Apples-MBP-4.attlocal.net>
2019-07-17 14:38:53 -0700

    Marnie was here

--- finalProject/ItemTableViewController.swift
+++ finalProject/ItemTableViewController.swift
@@ -56,7 +56,7 @@ class ItemTableViewController: UITableViewController {
                 }
             }
         }
-
+//MARNIE WAS HERE
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
