72e800534d4a6e4a53b70c491184c5ac47d1170c
Apple <apple@Apples-MBP-4.attlocal.net>
2019-07-18 10:03:16 -0700

    iteartion 1

--- finalProject/CategoryViewController.swift
+++ finalProject/CategoryViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController, UINavigationControllerDelegate {
+class CategoryViewController: UIViewController, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
 
     @IBOutlet weak var shopName: UITextField!
     
-    
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
     
+    let colors = ["Books",
+                  "Equiptment",
+                  "Fashion", "Food", "Furniture", "Music", "Technology", "Transportation", "Travel", "Other"  ]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+
+
     
     @IBAction func nextTapped(_ sender: UIButton) {
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
